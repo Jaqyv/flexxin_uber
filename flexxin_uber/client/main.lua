9f8a03687d88a8ffb0eb93edf4117498db412993
Flexxin <nealayeden68@gmail.com>
2025-08-27 19:23:10 -0500

    Update main.lua

--- flexxin_uber/client/main.lua
+++ flexxin_uber/client/main.lua
@@ -1,7 +1,7 @@
 ESX = exports["es_extended"]:getSharedObject()
 
-RegisterNetEvent('esx_uber:requestUber')
-AddEventHandler('esx_uber:requestUber', function()
+RegisterNetEvent('flexin_uber:requestUber')
+AddEventHandler('flexin_uber:requestUber', function()
     local input = lib.inputDialog('Uber Request', {'Current Location', 'Destination'})
     if not input or #input < 2 then
         ESX.ShowNotification('Uber request canceled.')
@@ -10,9 +10,10 @@ AddEventHandler('esx_uber:requestUber', function()
     TriggerServerEvent('esx_uber:sendUberRequest', input[1], input[2])
 end)
 
-RegisterNetEvent('esx_uber:setGPS')
-AddEventHandler('esx_uber:setGPS', function(coords)
+RegisterNetEvent('flexin_uber:setGPS')
+AddEventHandler('flexin_uber:setGPS', function(coords)
     SetNewWaypoint(coords.x, coords.y)
 
     ESX.ShowNotification('The GPS has been set to the requester\'s location.')
 end)
+
